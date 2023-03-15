fd53f5f9e2635414b46c016b38f0071cbe04fc06
Joshhacker254 <96799252+Joshhacker254@users.noreply.github.com>
2023-03-15 09:42:14 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,23 +16,23 @@ global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
 global.alivepic = `https://i.ibb.co/K0RtK0Z/IMG-20221021-WA0089.jpg `
-global.owner = ['94774071805'] //ur owner number
-global.ownername = "DarkTem" //ur owner name
-global.ytname = "YT: kavee" //ur yt chanel name
-global.socialm = "Dark nero" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['254735847430'] //ur owner number
+global.ownername = "joshhacker" //ur owner name
+global.ytname = "YT: joshhacker" //ur yt chanel name
+global.socialm = "josh" //ur github or insta name
+global.location = "Kenya, Nairobi" //ur location
 
 //bot bomdy 
-global.ownernomer = "94774071805" //ur number
+global.ownernomer = "254735847430" //ur number
 global.premium = ['94774071805'] //ur premium number
-global.botname = 'DarkNero Bot MD V7' //ur bot name
+global.botname = 'joshhacker Bot MD V7' //ur bot name
 global.linkz = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
+global.botscript = 'https://github.com/Joshhacker254/CheemsBot-MD6' //script link
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Sticker By" //ur sticker watermark packname
-global.author = "Cheems Bot\n\n\nA whatsapp bot developed by\n🦄Dream Guy Xeon\n\n\n\n\n\n\n\n       --------______-------\n\n\n\n\n\n\n\nContact No. : +916909137213" //ur sticker watermark author
-global.wm = "Dark Bot Inc." //ur watermark
+global.packname = "Sticker By joshhacker" //ur sticker watermark packname
+global.author = "josh Bot\n\n\nA whatsapp bot developed by\n🦄Dream Guy josh\n\n\n\n\n\n\n\n       --------______-------\n\n\n\n\n\n\n\nContact No. : +916909137213" //ur sticker watermark author
+global.wm = "josh Bot Inc." //ur watermark
 
 // Other
 global.sessionName = 'session'
@@ -49,7 +49,7 @@ global.mess = {
     group: 'Features Used Only For Groups!',
     private: 'Features Used Only For Private Chat!',
     bot: 'This feature could be used by bot only',
-    wait: 'In process...',
+    wait: 'fear women...',
     linkm: 'Where is the link?',
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
     nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
